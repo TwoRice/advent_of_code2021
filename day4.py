449308922b4d2e3a9e9cbcd7b078e895bb8d474f
TwoRice <2rimagowan@gmail.com>
2021-12-06 20:11:08 +0000

    refactor

--- day4.py
+++ day4.py
@@ -2,21 +2,22 @@ import re
 import numpy as np
 
 def parse_input():
+    collapse_whitespace = re.compile(r'\s+')
     with open('data/input4.txt', 'r') as f:
-        collapse_whitespace = re.compile(r'\s+')
         data = f.read().split('\n\n')
-        numbers = np.array(data[0].split(',')).astype(int)
-        board_data = [board.split('\n') for board in data[1:-1]]
-        boards = []
-        for board in board_data:
-            board = np.array([
-                np.array(collapse_whitespace.sub(' ', row).strip().split(' '))
-                for row in board
-            ]).astype(int)
-            boards.append(board)
-        boards = np.array(boards)
         
-        return numbers, boards
+    numbers = np.array(data[0].split(',')).astype(int)
+    board_data = [board.split('\n') for board in data[1:-1]]
+    boards = []
+    for board in board_data:
+        board = np.array([
+            np.array(collapse_whitespace.sub(' ', row).strip().split(' '))
+            for row in board
+        ]).astype(int)
+        boards.append(board)
+    boards = np.array(boards)
+
+    return numbers, boards
 
 if __name__ == "__main__":
     numbers, boards = parse_input()
