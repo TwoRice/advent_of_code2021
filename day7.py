682f1fc151f559abc97d2c2ae1d6f860665cea8b
TwoRice <2rimagowan@gmail.com>
2021-12-07 10:28:06 +0000

    refactored

--- day7.py
+++ day7.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-def simple_fuel_calc(crab, i):
-    return abs(crab - i)
-
-def real_fuel_calc(crab, i):
-    distance = simple_fuel_calc(crab, i)
+def fuel_calc(distance):
+    distance = abs(distance)
     return int((distance * (distance + 1)) / 2)
 
 def most_efficient_position(crabs, fuel_calc):
@@ -12,7 +9,7 @@ def most_efficient_position(crabs, fuel_calc):
     for i in range(crabs.min(), crabs.max()):
         fuel = 0
         for crab in crabs:
-            fuel += fuel_calc(crab, i)
+            fuel += fuel_calc(crab - i)
         if fuel < best_fuel:
             best_fuel = fuel
     
@@ -22,5 +19,5 @@ if __name__ == "__main__":
     with open('data/input7.txt', 'r') as f:
         crab_positions = np.array(f.read().split(','), dtype=int)
     
-    print(f'Part 1: {most_efficient_position(crab_positions, simple_fuel_calc)}')
-    print(f'Part 2: {most_efficient_position(crab_positions, real_fuel_calc)}')
+    print(f'Part 1: {most_efficient_position(crab_positions, abs)}')
+    print(f'Part 2: {most_efficient_position(crab_positions, fuel_calc)}')
